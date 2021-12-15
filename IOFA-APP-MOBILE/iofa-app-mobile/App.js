f2a8d57d00abd175daf8a5dbc2ed2fb9e3b0faad
Salem-Abderaouf <salamasalem84@gmail.com>
2021-12-15 19:53:05 +0100

    update App.js

--- IOFA-APP-MOBILE/iofa-app-mobile/App.js
+++ IOFA-APP-MOBILE/iofa-app-mobile/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
- import HomeScreen from './Screens/HomeScreen';
+import HomeScreen from './Screens/HomeScreen';
 
- import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Pump from './Screens/SubScreens/Pump';
 import PumpWatering from './Screens/SubScreens/PumpWatering';
@@ -14,44 +14,32 @@ import Maintenance from './Screens/SubScreens/Maintenance';
 import AerialSurvey from './Screens/SubScreens/AerialSurvey';
 import Surveillance from './Screens/SubScreens/Surveillance';
 import Aquaculture from './Screens/SubScreens/Aquaculture';
+import Loading from './Screens/Auth/Loading';
 
 const Stack = createNativeStackNavigator();
 
-
-
-
-
 export default function App() {
+
+  const globalHeaderOptions = {
+    headerShown: false,
+  }
+
   return (
-    
     <NavigationContainer>
-
       <Stack.Navigator>
-
-       <Stack.Screen name="Home" component= {HomeScreen} options={{headerShown: false}} />
-       <Stack.Screen name="PumbScreen" component= {Pump}  options={{headerShown: false}} />
-       <Stack.Screen name="PumpWatering" component= {PumpWatering} options={{headerShown: false}} />
-       <Stack.Screen name="DashbardAndMap" component= {DashbardAndMap} options={{headerShown: false}} />
-       <Stack.Screen name="WeatherTank" component= {WeatherTank} options={{headerShown: false}} />
-       <Stack.Screen name="SoilCondition" component= {SoilCondition} options={{headerShown: false}} />
-       <Stack.Screen name="Maintenance" component= {Maintenance} options={{headerShown: false}} />
-       <Stack.Screen name="AerialSurvey" component= {AerialSurvey} options={{headerShown: false}} />
-       <Stack.Screen name="Surveillance" component= {Surveillance} options={{headerShown: false}} />
-       <Stack.Screen name="Aquaculture" component= {Aquaculture} options={{headerShown: false}} />
-
-
+        <Stack.Screen name="Home" component={HomeScreen} options={globalHeaderOptions} />
+        <Stack.Screen name="PumbScreen" component={Pump} options={globalHeaderOptions} />
+        <Stack.Screen name="PumpWatering" component={PumpWatering} options={globalHeaderOptions} />
+        <Stack.Screen name="DashbardAndMap" component={DashbardAndMap} options={globalHeaderOptions} />
+        <Stack.Screen name="WeatherTank" component={WeatherTank} options={globalHeaderOptions} />
+        <Stack.Screen name="SoilCondition" component={SoilCondition} options={globalHeaderOptions} />
+        <Stack.Screen name="Maintenance" component={Maintenance} options={globalHeaderOptions} />
+        <Stack.Screen name="AerialSurvey" component={AerialSurvey} options={globalHeaderOptions} />
+        <Stack.Screen name="Surveillance" component={Surveillance} options={globalHeaderOptions} />
+        <Stack.Screen name="Aquaculture" component={Aquaculture} options={globalHeaderOptions} />
+        <Stack.Screen name="Loading" component={Loading} options={globalHeaderOptions} />
       </Stack.Navigator>
-    
-  </NavigationContainer>
-  // <HomeScreen/>
+    </NavigationContainer>
   );
 
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
- 
-  },
-});
