23aa0941dc283a089d0834e05de79b1a260c19ec
Salem-Abderaouf <salamasalem84@gmail.com>
2021-12-16 01:15:05 +0100

    update routs stack

--- IOFA-APP-MOBILE/iofa-app-mobile/App.js
+++ IOFA-APP-MOBILE/iofa-app-mobile/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import auth from "@react-native-firebase/auth"
+import database from "@react-native-firebase/database"
 import HomeScreen from './Screens/HomeScreen';
 
 import { NavigationContainer } from '@react-navigation/native';
@@ -15,6 +16,9 @@ import AerialSurvey from './Screens/SubScreens/AerialSurvey';
 import Surveillance from './Screens/SubScreens/Surveillance';
 import Aquaculture from './Screens/SubScreens/Aquaculture';
 import Loading from './Screens/Auth/Loading';
+import Login from './Screens/Auth/Login';
+import Register from './Screens/Auth/Register';
+import Reset from './Screens/Auth/PasswordReset';
 
 const Stack = createNativeStackNavigator();
 
@@ -24,20 +28,61 @@ export default function App() {
     headerShown: false,
   }
 
+  const [user, setUser] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const usersRef = database();
+    auth().onAuthStateChanged((user) => {
+      if (user) {
+        usersRef
+          .ref(`users/${user.uid}`)
+          .once('value')
+          .then((snapshot) => {
+            const userData = snapshot.val();
+            setUser(true);
+            setLoading(false);
+          })
+          .catch(() => {
+            setLoading(false)
+          });
+      }
+      else {
+        setUser(false);
+        setLoading(false);
+      }
+    })
+  }, [user])
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} options={globalHeaderOptions} />
-        <Stack.Screen name="PumbScreen" component={Pump} options={globalHeaderOptions} />
-        <Stack.Screen name="PumpWatering" component={PumpWatering} options={globalHeaderOptions} />
-        <Stack.Screen name="DashbardAndMap" component={DashbardAndMap} options={globalHeaderOptions} />
-        <Stack.Screen name="WeatherTank" component={WeatherTank} options={globalHeaderOptions} />
-        <Stack.Screen name="SoilCondition" component={SoilCondition} options={globalHeaderOptions} />
-        <Stack.Screen name="Maintenance" component={Maintenance} options={globalHeaderOptions} />
-        <Stack.Screen name="AerialSurvey" component={AerialSurvey} options={globalHeaderOptions} />
-        <Stack.Screen name="Surveillance" component={Surveillance} options={globalHeaderOptions} />
-        <Stack.Screen name="Aquaculture" component={Aquaculture} options={globalHeaderOptions} />
-        <Stack.Screen name="Loading" component={Loading} options={globalHeaderOptions} />
+        {loading ?
+          <Stack.Screen name="Loading" component={Loading} options={globalHeaderOptions} />
+          :
+          <>
+            {!user ?
+              <>
+                <Stack.Screen name="Login" component={Login} options={globalHeaderOptions} />
+                <Stack.Screen name="Register" component={Register} options={globalHeaderOptions} />
+                <Stack.Screen name="Reset" component={Reset} options={globalHeaderOptions} />
+              </>
+              :
+              <>
+                <Stack.Screen name="Home" component={HomeScreen} options={globalHeaderOptions} />
+                <Stack.Screen name="PumbScreen" component={Pump} options={globalHeaderOptions} />
+                <Stack.Screen name="PumpWatering" component={PumpWatering} options={globalHeaderOptions} />
+                <Stack.Screen name="DashbardAndMap" component={DashbardAndMap} options={globalHeaderOptions} />
+                <Stack.Screen name="WeatherTank" component={WeatherTank} options={globalHeaderOptions} />
+                <Stack.Screen name="SoilCondition" component={SoilCondition} options={globalHeaderOptions} />
+                <Stack.Screen name="Maintenance" component={Maintenance} options={globalHeaderOptions} />
+                <Stack.Screen name="AerialSurvey" component={AerialSurvey} options={globalHeaderOptions} />
+                <Stack.Screen name="Surveillance" component={Surveillance} options={globalHeaderOptions} />
+                <Stack.Screen name="Aquaculture" component={Aquaculture} options={globalHeaderOptions} />
+              </>
+            }
+          </>
+        }
       </Stack.Navigator>
     </NavigationContainer>
   );
