b3c9c96ebb45ccc8f20466e897b4db127ac618cd
Salem-Abderaouf <salamasalem84@gmail.com>
2022-01-01 02:24:11 +0100

    update style

--- IOFA-APP-MOBILE/iofa-app-mobile/src/Svg/LineChart .tsx
+++ IOFA-APP-MOBILE/iofa-app-mobile/src/Svg/LineChart .tsx
@@ -1,44 +1,37 @@
-import React, {useState, useEffect, useRef} from 'react'
-import { StyleSheet, Text, View, Image, Dimensions, Animated, FlatList, 
-        TouchableWithoutFeedback, TextInput, TouchableOpacity, ScrollView} from 'react-native'
-
-import  Svg, { Text as TextSvg ,G, Line, Circle, Path, Rect} from 'react-native-svg';
-
-//import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
+import React, { useState, useEffect, useRef } from 'react'
+import {
+    StyleSheet, Text, View, Image, Dimensions, Animated, FlatList,
+    TouchableWithoutFeedback, TextInput, TouchableOpacity, ScrollView
+} from 'react-native'
+import Svg, { Text as TextSvg, G, Line, Circle, Path, Rect } from 'react-native-svg';
 import Colors from '../depen/Colors';
 
 
-const LineChart  = ({
+const LineChart = ({
     CharpSvgColor,
     CharpSvgVerticalLinesColor,
-    CharpSvgPointsColor, 
-    CharpSvgPointsLnesColor, 
+    CharpSvgPointsColor,
+    CharpSvgPointsLnesColor,
     LineCharpData
-
-
-
 }) => {
-
-    const [yAxisLabels, setYAxisLabels ] = useState([]);
-
-
+    const [yAxisLabels, setYAxisLabels] = useState([]);
     useEffect(() => {
-        
+
         const yLineLabels = LineCharpData.map((item, index) => {
-            if (index ===0 ) {
+            if (index === 0) {
                 return Y_Min_Value
-            }else {
-           
-              return  Math.floor(Y_Min_Value + gabBetweenYaxValues * index);
+            } else {
+
+                return Math.floor(Y_Min_Value + gabBetweenYaxValues * index);
             }
         })
         setYAxisLabels(yLineLabels)
-      }, [])
+    }, [])
 
-    
-   
 
-    
+
+
+
 
 
     const x_axis_X1_point = 40
@@ -59,41 +52,39 @@ const LineChart  = ({
 
     const Animated_x_axis_width = useRef(new Animated.Value(x_axis_X1_point)).current;
     const Animated_y_axis_width = useRef(new Animated.Value(y_axis_Y1_point)).current;
-    const Animated_Circle_Radius = useRef(new Animated.Value(0), ).current;
+    const Animated_Circle_Radius = useRef(new Animated.Value(0)).current;
 
     useEffect(() => {
-        
+
         start_X_Y_axis_Animation();
         start_axis_Circle_Animation();
     }, [])
 
-    const  start_X_Y_axis_Animation = () => {
-        Animated.timing(Animated_x_axis_width,{
-            toValue : x_axis_X2_point,
-            duration : 4500,
-            useNativeDriver : true,
+    const start_X_Y_axis_Animation = () => {
+        Animated.timing(Animated_x_axis_width, {
+            toValue: x_axis_X2_point,
+            duration: 4500,
+            useNativeDriver: true,
         }).start();
-        Animated.timing(Animated_y_axis_width,{
-            toValue : y_axis_Y2_point,
-            duration : 4500,
-            useNativeDriver : true,
+        Animated.timing(Animated_y_axis_width, {
+            toValue: y_axis_Y2_point,
+            duration: 4500,
+            useNativeDriver: true,
         }).start();
     };
 
-    const  start_axis_Circle_Animation = () => {
-        Animated.timing(Animated_Circle_Radius ,{
-            toValue : 5,
-            duration : 4500,
-            useNativeDriver : true,
+    const start_axis_Circle_Animation = () => {
+        Animated.timing(Animated_Circle_Radius, {
+            toValue: 5,
+            duration: 4500,
+            useNativeDriver: true,
         }).start();
-        
+
     };
 
-    const  render_X_Y_axis = () => {
+    const render_X_Y_axis = () => {
         return (
             <G key='x-axis y-axis'>
-               
-
                 <AnimatedLine
                     key='x-axis'
                     x1={x_axis_X1_point}
@@ -103,8 +94,7 @@ const LineChart  = ({
                     stroke={CharpSvgColor}
                     strokeWidth={5}
                 />
-
-                 <AnimatedLine
+                <AnimatedLine
                     key='y-axis'
                     x1={y_axis_X1_point}
                     y1={Animated_y_axis_width}
@@ -113,203 +103,193 @@ const LineChart  = ({
                     stroke={CharpSvgColor}
                     strokeWidth={5}
                 />
-
-               
-
                 <Rect
-                 key='x-axis x1y2-circle'
-                 x= {y_axis_X1_point -8}
-                 y= {y_axis_Y2_point }
-                 width = {16}
-                 height = {4}
-                 fill= {CharpSvgColor}
-                 rx ={2}
-                 />
-
-                <AnimatedCircle
-                 key='x-axis x1y1-circle '
-                 cx= {x_axis_X1_point}
-                 cy= {x_axis_Y1_point}
-                 fill = {CharpSvgColor}
-                 r = {Animated_Circle_Radius}
-                 
+                    key='x-axis x1y2-circle'
+                    x={y_axis_X1_point - 8}
+                    y={y_axis_Y2_point}
+                    width={16}
+                    height={4}
+                    fill={CharpSvgColor}
+                    rx={2}
                 />
-                
                 <AnimatedCircle
-                 key='x-axis x2y2-circle'
-                 cx= {x_axis_X2_point}
-                 cy= {x_axis_Y2_point}
-                 fill = 'red'
-                 r = {Animated_Circle_Radius}
+                    key='x-axis x1y1-circle '
+                    cx={x_axis_X1_point}
+                    cy={x_axis_Y1_point}
+                    fill={CharpSvgColor}
+                    r={Animated_Circle_Radius}
+
                 />
-
-              
-                
-
-                
+                <AnimatedCircle
+                    key='x-axis x2y2-circle'
+                    cx={x_axis_X2_point}
+                    cy={x_axis_Y2_point}
+                    fill='red'
+                    r={Animated_Circle_Radius}
+                />
             </G>
         )
     }
 
-    const render_X_Labels_Tricks =() => {
+    const render_X_Labels_Tricks = () => {
         return LineCharpData.map((item, indexI) => {
             return (
                 <G
-                 key = {`x-axis labels Ticks ${item} ${indexI}`}>
+                    key={`x-axis labels Ticks ${item} ${indexI}`}>
                     <Line
-                     key = 'x-axis-Ticks'
-                     x1={x_axis_X1_point +22 +(30 * indexI)}
-                     y1={x_axis_Y1_point }
-                     x2={x_axis_X1_point +22 +(30 * indexI) }
-                     y2={x_axis_Y1_point+10}
-                     stroke={CharpSvgColor}
-                     strokeWidth={3}>
-    
-    
+                        key='x-axis-Ticks'
+                        x1={x_axis_X1_point + 22 + (30 * indexI)}
+                        y1={x_axis_Y1_point}
+                        x2={x_axis_X1_point + 22 + (30 * indexI)}
+                        y2={x_axis_Y1_point + 10}
+                        stroke={CharpSvgColor}
+                        strokeWidth={3}>
+
+
                     </Line>
                     <TextSvg
-                      x={x_axis_X1_point +22 +(30 * indexI)}
-                      y={x_axis_Y1_point + 20}
-                      fill={CharpSvgColor}
-                      textAnchor='middle'
+                        x={x_axis_X1_point + 22 + (30 * indexI)}
+                        y={x_axis_Y1_point + 20}
+                        fill={CharpSvgColor}
+                        textAnchor='middle'
                     >
                         {item.month}
-                   </TextSvg>
+                    </TextSvg>
                 </G>
             )
-    
+
         })
 
-       
-    } 
+
+    }
 
     const Y_Min_Value = 0;
     const Y_Max_Value = Math.max.apply(Math, LineCharpData.map((item) => item.value));
-    const Gap_Between_Y_Labels_Tricks = (Y_Max_Value -  Y_Min_Value) / 9
+    const Gap_Between_Y_Labels_Tricks = (Y_Max_Value - Y_Min_Value) / 9
 
     const gabBetweenYaxValues = (Y_Max_Value - Y_Min_Value) / 11
 
 
 
-    const render_Y_Labels_Tricks =() => {
+    const render_Y_Labels_Tricks = () => {
 
-       return yAxisLabels.map((item, index) => {
+        return yAxisLabels.map((item, index) => {
             return (
                 <G
-                 key = {`y-axis labels Ticks ${item} ${index}`}>
+                    key={`y-axis labels Ticks ${item} ${index}`}>
 
-                      <Line
-                     key = {`y-axis Ticks vertical-line ${item} ${index}`}
-                     x1={x_axis_X1_point + 1000} //End  
-                     y1={230 - ( ((Gap_Between_Y_Labels_Tricks -5 )*index))}
-                     x2={x_axis_X1_point+5 }
-                     y2={230 - ( ((Gap_Between_Y_Labels_Tricks -5 )*index))}
-                     stroke= {CharpSvgVerticalLinesColor}
-                     strokeWidth={3}>
-    
-    
+                    <Line
+                        key={`y-axis Ticks vertical-line ${item} ${index}`}
+                        x1={x_axis_X1_point + 1000} //End  
+                        y1={230 - (((Gap_Between_Y_Labels_Tricks - 5) * index))}
+                        x2={x_axis_X1_point + 5}
+                        y2={230 - (((Gap_Between_Y_Labels_Tricks - 5) * index))}
+                        stroke={CharpSvgVerticalLinesColor}
+                        strokeWidth={3}>
+
+
                     </Line>
 
                     <Line
-                     key = {`y-axis Ticks ${item} ${index}`}
-                     x1={x_axis_X1_point -7}
-                     y1={230 - ( ((Gap_Between_Y_Labels_Tricks -5 )*index))}
-                     x2={x_axis_X1_point+2 }
-                     y2={230 - ( ((Gap_Between_Y_Labels_Tricks -5 )*index))}
-                     stroke={CharpSvgColor}
-                     strokeWidth={3}>
-    
-    
+                        key={`y-axis Ticks ${item} ${index}`}
+                        x1={x_axis_X1_point - 7}
+                        y1={230 - (((Gap_Between_Y_Labels_Tricks - 5) * index))}
+                        x2={x_axis_X1_point + 2}
+                        y2={230 - (((Gap_Between_Y_Labels_Tricks - 5) * index))}
+                        stroke={CharpSvgColor}
+                        strokeWidth={3}>
+
+
                     </Line>
 
-                    
+
 
                     <TextSvg
-                      x={x_axis_X1_point -24}
-                      y={232 - ( ((Gap_Between_Y_Labels_Tricks -5 )*index))}
-                      fill={CharpSvgColor}
-                      textAnchor='middle'
-                      fontSize = '14'
-                      fontWeight  ='700'
+                        x={x_axis_X1_point - 24}
+                        y={232 - (((Gap_Between_Y_Labels_Tricks - 5) * index))}
+                        fill={CharpSvgColor}
+                        textAnchor='middle'
+                        fontSize='14'
+                        fontWeight='700'
                     >
                         {item}
-                   </TextSvg>
-                   
+                    </TextSvg>
+
                 </G>
             )
         })
 
-      
+
 
     }
 
     const getDataAxPath = () => {
-        const MaxValueInYax = yAxisLabels[ yAxisLabels.length - 1]
+        const MaxValueInYax = yAxisLabels[yAxisLabels.length - 1]
         if (MaxValueInYax) {
             let d = ''
-            LineCharpData.map((item,index)=> {
-              let xPointInside = x_axis_X1_point +22+ (30 * index)
-  
-              let yPointInside = (MaxValueInYax - item.value)
-                                  *(Gap_Between_Y_Labels_Tricks / 30) +45
-              if (index  === 0 ) {
-                  d+= `M${xPointInside} ${yPointInside}`
-              }else {
-                  d+= `L${xPointInside} ${yPointInside}`
-              }
-          } )
+            LineCharpData.map((item, index) => {
+                let xPointInside = x_axis_X1_point + 22 + (30 * index)
 
-          return d
+                let yPointInside = (MaxValueInYax - item.value)
+                    * (Gap_Between_Y_Labels_Tricks / 30) + 45
+                if (index === 0) {
+                    d += `M${xPointInside} ${yPointInside}`
+                } else {
+                    d += `L${xPointInside} ${yPointInside}`
+                }
+            })
 
-        }          
+            return d
+
+        }
     }
 
     const render_Graph_Path = () => {
         const dPath = getDataAxPath()
-         return (
+        return (
             <Path
-            d= {dPath}
-            strokeWidth = {2.5}
-            stroke = {CharpSvgPointsLnesColor}
+                d={dPath}
+                strokeWidth={2.5}
+                stroke={CharpSvgPointsLnesColor}
 
             />
         )
     }
-    
+
 
     const render_Graph_Path_Cercles = () => {
-        const MaxValueInYax = yAxisLabels[ yAxisLabels.length - 1]
+        const MaxValueInYax = yAxisLabels[yAxisLabels.length - 1]
         if (MaxValueInYax) {
-            
-           return LineCharpData.map((item,index)=> {
-              let xPointInside = x_axis_X1_point +22+ (30 * index)
-  
-              let yPointInside = (MaxValueInYax - item.value)
-                                  *(Gap_Between_Y_Labels_Tricks / 30) +45
 
-            return (
-               
+            return LineCharpData.map((item, index) => {
+                let xPointInside = x_axis_X1_point + 22 + (30 * index)
 
-                <G
-                key ={`Linear chart G Circles ${index}`}
-                >
-                     <Rect
-                 key='x-axis x2y2-circle'
-                 x= {xPointInside -4}
-                 y= {yPointInside -4}
-                 width = {9}
-                 height = {9}
-                 fill= {CharpSvgPointsColor}
-                 rx ={2}
-                 />
-                     
-                </G>
-            )
-             
-          } )
+                let yPointInside = (MaxValueInYax - item.value)
+                    * (Gap_Between_Y_Labels_Tricks / 30) + 45
+
+                return (
 
 
-        }   
+                    <G
+                        key={`Linear chart G Circles ${index}`}
+                    >
+                        <Rect
+                            key='x-axis x2y2-circle'
+                            x={xPointInside - 4}
+                            y={yPointInside - 4}
+                            width={9}
+                            height={9}
+                            fill={CharpSvgPointsColor}
+                            rx={2}
+                        />
+
+                    </G>
+                )
+
+            })
+
+
+        }
     }
 
 
@@ -317,8 +297,8 @@ const LineChart  = ({
 
     return (
         <ScrollView horizontal>
-                <TouchableWithoutFeedback>
-                
+            <TouchableWithoutFeedback>
+
                 <Svg height="300" width="450" style={styles.GrapgContainerSvg}>
                     {
                         render_X_Y_axis()
@@ -331,34 +311,32 @@ const LineChart  = ({
                         render_Y_Labels_Tricks()
                     }
 
-                    { 
+                    {
                         render_Graph_Path()
                     }
-                    { 
+                    {
                         render_Graph_Path_Cercles()
                     }
 
-                    
+
 
                 </Svg>
 
-                </TouchableWithoutFeedback>
+            </TouchableWithoutFeedback>
 
-                </ScrollView>
+        </ScrollView>
     )
 }
 
-export default LineChart 
+export default LineChart
 
 const styles = StyleSheet.create({
-    
-    GrapgContainerSvg : {
-        backgroundColor: Colors.light,
-        borderRadius :10,
-        padding : 10,
-        paddingVertical :50,
-        paddingRight : 30,
+    GrapgContainerSvg: {
+        alignSelf: 'center',
+        backgroundColor: Colors.dark,
+        borderRadius: 10,
+        padding: 10,
+        width: Dimensions.get('window').width - 30,
 
     },
-
 })
